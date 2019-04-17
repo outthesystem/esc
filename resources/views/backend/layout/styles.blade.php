c86b07380b1cde97d047e263926da7d6282ceb5d
Miguel San Martin <jet.market.dev@gmail.com>
2019-04-17 15:14:59 -0300

    message

--- resources/views/backend/layout/styles.blade.php
+++ resources/views/backend/layout/styles.blade.php
@@ -1,11 +1,11 @@
-    <script src="{{ asset('backend/css/vendor/jquery-jvectormap-1.2.2.css') }}" rel="stylesheet" type="text/css" />
-    <script src="{{ asset('backend/css/vendor/fullcalendar.min.css') }}" rel="stylesheet" type="text/css" />
-    <script src="{{ asset('backend/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
-    <script src="{{ asset('backend/css/fontawesome.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('public/backend/css/vendor/jquery-jvectormap-1.2.2.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('public/backend/css/vendor/fullcalendar.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('public/backend/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('public/backend/css/fontawesome.min.css') }}" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.css">
-    <script src="{{ asset('backend/css/app.min.css') }}" rel="stylesheet" type="text/css" />
-    <script src="{{ asset('backend/css/vendor/dataTables.bootstrap4.css') }}" rel="stylesheet" type="text/css" />
-    <script src="{{ asset('backend/css/vendor/responsive.bootstrap4.css') }}" rel="stylesheet" type="text/css" />
-    <script src="{{ asset('backend/css/vendor/buttons.bootstrap4.css') }}" rel="stylesheet" type="text/css" />
-    <script src="{{ asset('backend/css/vendor/select.bootstrap4.css') }}" rel="stylesheet" type="text/css" />
-    <script src="{{ asset('backend/css/main.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('public/backend/css/app.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('public/backend/css/vendor/dataTables.bootstrap4.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('public/backend/css/vendor/responsive.bootstrap4.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('public/backend/css/vendor/buttons.bootstrap4.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('public/backend/css/vendor/select.bootstrap4.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('public/backend/css/main.css') }}" rel="stylesheet" type="text/css" />
